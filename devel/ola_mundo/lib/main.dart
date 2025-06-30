a43a6581e222a03b6673aa9cf7103c7c86bc7e24
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2025-06-30 08:29:22 -0300

    m,n

--- devel/ola_mundo/lib/main.dart
+++ devel/ola_mundo/lib/main.dart
@@ -7,16 +7,31 @@ void main() { //retorno vazio
   //widget em flutter é uma ferramenta 
   // tudo no flutter é um widget incluseive o aplicativo
     MaterialApp(
-      home: Center( 
-        child:  Text('olá mundo', style: TextStyle(fontSize: 10),), 
+      home: 
+      
+      Scaffold(
+        appBar: AppBar(
+          title: Text('aplicativo olá mundo'),
         ),
-    )
-    //Material APP é o método construtor
+        body: Center(
+        child:  Text('olá mundo',
+         style: TextStyle(fontSize:50, color:Colors.black),
+         ), 
+        ),
+    ),
+      )
+  );
+  }
+  //Material APP é o método construtor
     //é a principal widget do flutter
     //text para usar um texto
     //center vai centralizar o texto, e vai esperar o child que é o filho dele 
     // após cada widget por uma virgula no final, porque vamos colocar vários widgets
     // o style é usado como um css
     // font size é usado no tamanho da fonte 
-  ); 
-}
+    // os parametros  opcionais estão entre chaves 
+    //a classe material que está importada cuida do ajuste de telas para diferentes dispositivos 
+   // mostrar a material.io
+   //scafoold é para criar uma tela 
+   // appbar da a barra dom aplicativo
+   // title para dar o título do aplicativo
